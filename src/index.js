b3d49aa9899ef12bf5e7e9a6fff46d72b7167159
oleksandr anishchenko <anishchenko.alexandr@gmail.com>
2023-07-31 19:32:15 +0300

    feat: pass 910 tests

--- src/index.js
+++ src/index.js
@@ -1,19 +1,31 @@
 module.exports = function toReadable (number) {
   const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
   const tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
   const numLength = (String(number).length);
   
-  if (numLength === 1) {
+  if (numLength === 1 || number <= 20) { // 0 - 20
     return units[number];
-  }else if (numLength === 2) {
-    if (number <= 20) return units[number];
+  }else if (numLength === 2) { // 21 - 99
+    const firstSign = Math.floor(number / 10); // how many dozens in number
+    const secondSign = number % 10; // how many units in number
 
-    const firstSign = Math.floor(number / 10);
-    const secondSign = number % 10;
-    return `${tens[firstSign - 1]} ${units[secondSign]}`;
-
-
+    if (secondSign === 0) {
+      return tens[firstSign - 1];
+    }else {
+      return `${tens[firstSign - 1]} ${units[secondSign]}`;
+    }
+  }else if (numLength === 3) { // 100 - 999
+    const firstSign = Math.floor(number / 100);
+    const secondSign = Math.floor((number % 100) / 10);
+    const thirdSign = number % 10;
+    // console.log(firstSign, secondSign, thirdSign)
+    if (secondSign === 0) {
+      return `${units[firstSign]} hundred ${units[thirdSign]}`; // 101
+    }else if (thirdSign === 0) {
+      return `${units[firstSign]} hundred ${tens[secondSign - 1]}`; // 120
+    }else {
+      return `${units[firstSign]} hundred ${tens[secondSign - 1]} ${units[thirdSign]}`; // 123
+    }
   }
   return numLength;
 
