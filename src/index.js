7db48340c62ca654cc301ef275bc13f97b944ead
oleksandr anishchenko <anishchenko.alexandr@gmail.com>
2023-07-31 18:30:56 +0300

    feat: add solution from 0 to 99, except dozens

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
+  const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+  const tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  const numLength = (String(number).length);
   
+  if (numLength === 1) {
+    return units[number];
+  }else if (numLength === 2) {
+    if (number <= 20) return units[number];
+
+    const firstSign = Math.floor(number / 10);
+    const secondSign = number % 10;
+    return `${tens[firstSign - 1]} ${units[secondSign]}`;
+
+
+  }
+  return numLength;
+
 }
